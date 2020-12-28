58939a4a7b3cbcb44904602b5b86a9e0eb463f14
Ignacio Barrio <ignaciobfp@gmail.com>
2020-12-28 17:26:27 +0100

    Fix datatype of aux variables. Proper use of interrupts.

--- pwm2duty.ino
+++ pwm2duty.ino
@@ -1,51 +1,51 @@
+#define SIGNAL_INVERTED 1
+
 const int signalpin = 2;
 
 float duty_cycle = 0.0;
 
-volatile int prev_time_rising = 0;
-volatile int current_time_rising = 0;
-volatile int last_event_time = 0;
+volatile unsigned long prev_time_rising = 0;
+volatile unsigned long current_time_rising = 0;
+volatile unsigned long last_event_time = 0;
 
-volatile int hill_duration = 0;
-volatile int total_duration = 0;
+volatile unsigned long hill_duration = 0;
+volatile unsigned long total_duration = 0;
 
 void rising() {
-  noInterrupts();
   last_event_time = micros();
   current_time_rising = last_event_time;
   total_duration = current_time_rising - prev_time_rising;
   prev_time_rising = current_time_rising;
-  interrupts();
+  attachInterrupt(digitalPinToInterrupt(signalpin), falling, FALLING);
 }
  
 void falling() {
-  noInterrupts();
   last_event_time = micros();
   hill_duration = last_event_time - prev_time_rising;
-  interrupts();
+  attachInterrupt(digitalPinToInterrupt(signalpin), rising, RISING);
 }
  
 void setup() {
   Serial.begin(115200);
+  pinMode(signalpin, INPUT_PULLUP);
   //We use function digitalPinToInterrupt for convenience. On arduino Uno, interrupt 0 is pin D2
   attachInterrupt(digitalPinToInterrupt(signalpin), rising, RISING);
-  attachInterrupt(digitalPinToInterrupt(signalpin), falling, FALLING);
 }
  
 void loop() {
-  delay(500);
+  delay(100);
 
   noInterrupts();
   //For worst case on a 50hz signal 1/50*10^6 gives 20.000 micros
-  if (micros() - last_event_time > 21000){
-    duty_cycle = digitalRead(signalpin);
+  if (micros() - last_event_time > 21000 || hill_duration < 1000 || total_duration < 1000){
+    duty_cycle = (float) digitalRead(signalpin);
   } else {
-    duty_cycle = hill_duration / total_duration;
+    duty_cycle = (float) hill_duration / total_duration;
   }
+  if(SIGNAL_INVERTED)
+    duty_cycle = 1 - duty_cycle;
   interrupts();
   //Serial.print("Duty: ");
   Serial.println(duty_cycle*100, 0);
-
-  
 }
  
